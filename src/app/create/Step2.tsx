4c0659fc4cdce3704a9e3308673d9bc21989808e
edward <bepyan@naver.com>
2023-02-27 18:06:09 +0900

    🚧  car park detail layout

--- src/app/create/Step2.tsx
+++ src/app/create/Step2.tsx
@@ -1,7 +1,22 @@
+import { create } from 'zustand';
+
 import { Icons } from '~/ui/Icons';
 import { Input } from '~/ui/Input';
 
+const useCarParkDetail = create<{
+  show: boolean;
+  showCarParkDetail: () => void;
+  hideCarParkDetail: () => void;
+}>((set) => ({
+  show: false,
+  showCarParkDetail: () => set((state) => ({ ...state, show: true })),
+  hideCarParkDetail: () => set((state) => ({ ...state, show: false })),
+}));
+
 export default function Step2() {
+  const show = useCarParkDetail((s) => s.show);
+  const showCarParkDetail = useCarParkDetail((s) => s.showCarParkDetail);
+
   return (
     <>
       <div className='space-y-3 p-container pt-3'>
@@ -16,7 +31,10 @@ export default function Step2() {
             <span className='text-sm font-bold'>근처 주차장 추천</span>
           </div>
           <div className='mt-container rounded-xl border'>
-            <div className='flex items-center justify-between p-container'>
+            <div
+              className='flex items-center justify-between p-container'
+              onClick={showCarParkDetail}
+            >
               <div>
                 <p className='font-bold'>오라이 주차장</p>
                 <p className='mt-1 text-sm'>서울 송파구 마청동 215-0</p>
@@ -26,6 +44,22 @@ export default function Step2() {
           </div>
         </div>
       </div>
+      {show && <CarParkDetail />}
     </>
+  );
+}
+
+function CarParkDetail() {
+  const hideCarParkDetail = useCarParkDetail((s) => s.hideCarParkDetail);
+
+  return (
+    <div className='fixed inset-0 z-40 bg-white'>
+      <div className='fixed top-1.5 left-2.5'>
+        <div className='flex h-9 w-9 items-center justify-center rounded-full bg-white'>
+          <Icons.ChevronLeftIcon className='h-6 w-6' onClick={hideCarParkDetail} />
+        </div>
+      </div>
+      <div className='h-48 bg-red-100' />
+    </div>
   );
 }
