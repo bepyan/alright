0df61cdd335ef83c6c6c4bb0b69dad6e8068b782
edward <bepyan@naver.com>
2023-02-27 21:50:10 +0900

    💫 point down button action opacity

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { cn } from '~/lib/utils';
 
 export const buttonVariants = cva(
-  'active:opacity-click transition-opacity inline-flex items-center justify-center rounded-lg text-lg font-bold select-none focus:outline-none disabled:opacity-50 disabled:pointer-events-none',
+  'active:opacity-90 transition-opacity inline-flex items-center justify-center rounded-lg text-lg font-bold select-none focus:outline-none disabled:opacity-50 disabled:pointer-events-none',
   {
     variants: {
       variant: {
