bb5e32c122df5c05dbdb84a061defdf80e288b87
edward <bepyan@naver.com>
2023-02-28 22:11:31 +0900

    🎨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,13 @@ import '~/styles/globals.css';
 
 import React from 'react';
 
-import { cn } from '~/lib/utils';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko' className='bg-white'>
       <head>
         <title>오리이</title>
       </head>
-      <body className={cn('w-full bg-zinc-50 font-sans text-slate-900 antialiased')}>
+      <body className='w-full bg-zinc-50 font-sans text-slate-900 antialiased'>
         <div className='container min-h-screen bg-white'>{children}</div>
       </body>
     </html>
