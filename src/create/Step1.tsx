4ac114e017878958124e6ac3436dd67db96b2863
edward <bepyan@naver.com>
2023-03-01 14:25:03 +0900

    🚧 dummy place

--- src/create/Step1.tsx
+++ src/create/Step1.tsx
@@ -10,8 +10,23 @@ import StepNav from '~/ui/StepNav';
 
 import { useCreate } from './state';
 
+const dummyPlace = {
+  address_name: '서울 용산구 한강로2가 98-3',
+  category_group_code: 'FD6',
+  category_group_name: '음식점',
+  category_name: '음식점 > 술집 > 와인바',
+  distance: '',
+  id: '19950533191234',
+  phone: '',
+  place_name: '오라이샴팡',
+  place_url: 'http://place.map.kakao.com/1995053319',
+  road_address_name: '서울 용산구 한강대로46길 15',
+  x: '126.971150136434',
+  y: '37.5312061331283',
+};
+
 export default function Step1() {
-  const [placeList, setPlaceList] = useState<Place[]>([]);
+  const [placeList, setPlaceList] = useState<Place[]>([dummyPlace]);
 
   const { searchHandler } = useSearch((searchValue) => {
     if (!searchValue) return;
