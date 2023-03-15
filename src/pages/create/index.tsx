dd79c535e59e6c48a60996eb9ab87f8d3abbcdb2
edward <bepyan@naver.com>
2023-03-15 20:28:11 +0900

    🚚 create to components/create

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,8 +1,8 @@
-import HeaderNavContent from '~/create/HeaderNavContent';
-import { useCreate } from '~/create/state';
-import Step1 from '~/create/Step1';
-import Step2 from '~/create/Step2';
-import Step3 from '~/create/Step3';
+import HeaderNavContent from '~/components/create/HeaderNavContent';
+import { useCreate } from '~/components/create/state';
+import Step1 from '~/components/create/Step1';
+import Step2 from '~/components/create/Step2';
+import Step3 from '~/components/create/Step3';
 import HeaderNav from '~/ui/HeaderNav';
 
 export default function Page() {
