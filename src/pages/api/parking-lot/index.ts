dd95dd585ff2655c1f01c73d31de4361229e5d07
jyheo <jy119.heo@partner.samsung.com>
2023-03-29 18:38:54 +0900

    parking-lot api 기본기능 (Get Post) 추가 Validation 없음 기본적인 Save/Load 만 가능

--- src/pages/api/parking-lot/index.ts
+++ src/pages/api/parking-lot/index.ts
@@ -1,23 +1,60 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Schema, model, connect } from 'mongoose';
+import { Alright } from './scheme/parking-lot'
+import crypto from 'crypto';
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Response>) {
+  res: NextApiResponse<any>) {
 
   switch (req.method) {
     case 'GET':
-      res.status(200).json({
-        method: 'GET'
+      const hashCode = req.query.hashCode as string;
+      getParkingLot(hashCode).then(function (foundAlright) {
+        res.status(200).json({
+          data: foundAlright,
+          hashCode
+        })
       })
+
       break;
     case 'POST':
-      res.status(200).json({
-        method: 'POST'
+      createParkingLot(req.body).then(function (hashCode) {
+        res.status(200).json({
+          message: "Save Complete!",
+          hashCode: hashCode
+        })
       })
       break;
   }
 }
 
-type Response = {
-  method: string
+async function getParkingLot(hashCode: string) {
+  const uri = "mongodb+srv://gjwodud119:young8291@cluster0.w9a9rkz.mongodb.net/?retryWrites=true&w=majority";
+  await connect(uri)
+  var foundAlright
+  await Alright.findOne({
+    hashCode: hashCode
+  }).exec().then(alright => {
+    foundAlright = alright
+  })
+
+  return foundAlright
+}
+
+async function createParkingLot(body: any) {
+  const uri = "mongodb+srv://gjwodud119:young8291@cluster0.w9a9rkz.mongodb.net/?retryWrites=true&w=majority";
+  await connect(uri)
+
+  const companyAddress = body.company.address_name
+  const currentDate = new Date()
+  const hashCode = crypto.createHash('md5').update(companyAddress + currentDate.toString()).digest('hex').substring(0, 8)
+  await Alright.create({
+    address: companyAddress,
+    createdDate: currentDate,
+    hashCode,
+    parkingLots: body.selectedCarParkList
+  })
+
+  return hashCode
 }
