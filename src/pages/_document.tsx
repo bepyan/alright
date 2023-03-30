8241a49c73ac1fa0467e3db3e7b6634d909f1d29
edward <bepyan@naver.com>
2023-03-30 23:51:55 +0900

    ⚰️ fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,9 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default function MyDocument() {
   return (
     <Html lang='ko' className='bg-white'>
-      <Head>
-        <link
-          href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
       <body className='w-full overflow-hidden bg-zinc-50 font-sans text-slate-900 antialiased'>
         <div className='container min-h-screen bg-white font-sans'>
           <Main />
