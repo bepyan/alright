84fc779b1b6d2695d390a27ad67b9edc298cb003
edward <bepyan@naver.com>
2023-04-15 17:20:20 +0900

    🎨 refactor code layout

--- src/components/park/CarParkDetail.tsx
+++ src/components/park/CarParkDetail.tsx
@@ -1,7 +1,6 @@
 import { isMobile } from 'react-device-detect';
 
 import { useCurrentGeolocation } from '~/lib/useGeolocation';
-import { ParkingLotInfo } from '~/models/alright';
 import BackButton from '~/ui/BackButton';
 import Button from '~/ui/Button';
 import HeaderNav from '~/ui/HeaderNav';
@@ -19,26 +18,81 @@ export default function ParkDetailLayout() {
       <HeaderNav className='h-14'>
         <BackButton onClick={hideCarParkDetail} />
       </HeaderNav>
-      {selectedCarPark && <ParkDetail selectedCarPark={selectedCarPark} />}
+      {selectedCarPark && <ParkDetail />}
     </div>
   );
 }
 
-function ParkDetail({ selectedCarPark }: { selectedCarPark: ParkingLotInfo }) {
-  const company = useCarParkDetail((s) => s.company);
+function ParkDetail() {
+  return (
+    <>
+      <HeaderInfo />
+
+      <DefaultInfo />
+
+      <div className='bg-white px-container'>
+        <div className='h-[1px] bg-al-border' />
+        <FeeInfo />
+        <div className='h-[1px] bg-al-border' />
+        <TimeInfo />
+        <div className='h-[1px] bg-al-border' />
+        <OtherInfo />
+      </div>
+    </>
+  );
+}
+function HeaderInfo() {
   const currentPosition = useCurrentGeolocation();
+  const selectedCarPark = useCarParkDetail((s) => s.selectedCarPark)!;
+
+  const navToPark = () => {
+    console.log(currentPosition);
+
+    if (isMobile) {
+      if (currentPosition) {
+        location.href = `kakaomap://route?sp=${currentPosition.lat},${currentPosition.lng}&ep=${selectedCarPark.y},${selectedCarPark.x}&by=CAR`;
+      } else {
+        location.href = `kakaomap://place?id=${selectedCarPark.id}`;
+      }
+    } else {
+      window.open(`https://map.kakao.com/link/to/${selectedCarPark.id}`, '_ blank');
+    }
+  };
+
+  return (
+    <div className='flex flex-col bg-al-gray-100 p-container'>
+      <h1 className='mt-5 self-center text-2xl font-bold'>{selectedCarPark.place_name}</h1>
+      <RealtimeParkInfo />
+      <Button onClick={navToPark}>길찾기</Button>
+    </div>
+  );
+}
+
+function RealtimeParkInfo() {
   const isSelectedPublishCarPark = useCarParkDetail((s) => s.computed.isSelectedPublishCarPark);
 
-  // const { data, isLoading, isError, error } = useQuery({
-  //   queryKey: ['carParkDetail', selectedCarPark?.id],
-  //   queryFn: () =>
-  //     selectedCarPark &&
-  //     axios({
-  //       url: '/api/parking-lot/public',
-  //       method: 'GET',
-  //       params: { address: selectedCarPark.address_name },
-  //     }).then((res) => res.data),
-  // });
+  if (!isSelectedPublishCarPark) {
+    return <div className='h-10' />;
+  }
+
+  return (
+    <div className='mt-5 mb-3 flex h-[100px] items-center rounded-lg bg-white'>
+      <div className='flex-1 text-center font-bold text-al-blue'>
+        <div className='text-2xl'>{117}</div>
+        <div className='text-sm'>주차 여유</div>
+      </div>
+      <Separator height={40} />
+      <div className='flex-1 text-center font-bold text-al-slate'>
+        <div className='text-2xl'>{217}</div>
+        <div className='text-sm'>전체</div>
+      </div>
+    </div>
+  );
+}
+
+function DefaultInfo() {
+  const company = useCarParkDetail((s) => s.company);
+  const selectedCarPark = useCarParkDetail((s) => s.selectedCarPark)!;
 
   const navToKakaoMap = () => {
     if (isMobile) {
@@ -51,18 +105,6 @@ function ParkDetail({ selectedCarPark }: { selectedCarPark: ParkingLotInfo }) {
     }
   };
 
-  const navToPark = () => {
-    if (isMobile) {
-      if (currentPosition) {
-        location.href = `kakaomap://route?sp=${currentPosition.lat},${currentPosition.lng}&ep=${selectedCarPark.y},${selectedCarPark.x}&by=CAR`;
-      } else {
-        location.href = `kakaomap://place?id=${selectedCarPark.id}`;
-      }
-    } else {
-      window.open(`https://map.kakao.com/link/to/${selectedCarPark.id}`, '_ blank');
-    }
-  };
-
   const navToCompany = () => {
     if (isMobile) {
       location.href = `kakaomap://route?sp=${selectedCarPark.y},${selectedCarPark.x}&ep=${company.y},${company.x}&by=FOOT`;
@@ -72,112 +114,73 @@ function ParkDetail({ selectedCarPark }: { selectedCarPark: ParkingLotInfo }) {
   };
 
   return (
-    <>
-      <div className='flex flex-col bg-al-gray-100 p-container'>
-        <h1 className='mt-5 self-center text-2xl font-bold'>{selectedCarPark.place_name}</h1>
-        {isSelectedPublishCarPark && (
-          <div className='mt-5 flex h-[100px] items-center rounded-lg bg-white'>
-            <div className='flex-1 text-center font-bold text-al-blue'>
-              <div className='text-2xl'>{117}</div>
-              <div className='text-sm'>주차 여유</div>
-            </div>
-            <Separator height={40} />
-            <div className='flex-1 text-center font-bold text-al-slate'>
-              <div className='text-2xl'>{217}</div>
-              <div className='text-sm'>전체</div>
-            </div>
-          </div>
-        )}
-        <Button className='mt-3' onClick={navToPark}>
-          길찾기
+    <div className='bg-white p-container space:mt-4'>
+      {selectedCarPark.phone && (
+        <div className='flex items-center'>
+          <Icons.Phone className='text-al-slate' />
+          <div className='ml-1 text-sm'>{selectedCarPark.phone}</div>
+          <Button className='ml-auto rounded-full' size='xs' variant='subtle'>
+            전화하기
+          </Button>
+        </div>
+      )}
+      <div className='flex items-center'>
+        <Icons.Location className='text-al-slate' />
+        <div className='ml-1 text-sm'>{selectedCarPark.address_name}</div>
+        <Button className='ml-auto rounded-full' size='xs' variant='subtle' onClick={navToKakaoMap}>
+          지도보기
         </Button>
       </div>
+      <div className='flex items-center'>
+        <Icons.Route className='text-al-slate' />
+        <div className='ml-1 text-sm'>걸어서 업체가는 방법</div>
+        <Button className='ml-auto rounded-full' size='xs' variant='subtle' onClick={navToCompany}>
+          카카오맵
+        </Button>
+      </div>
+    </div>
+  );
+}
 
-      <div className='bg-white py-2'>
-        {selectedCarPark.phone && (
-          <div className='flex items-center py-2 px-5'>
-            <Icons.Phone className='text-al-slate' />
-            <div className='ml-1 text-sm'>{selectedCarPark.phone}</div>
-            <Button className='ml-auto rounded-full' size='xs' variant='subtle'>
-              전화하기
-            </Button>
+function FeeInfo() {
+  const selectedCarPark = useCarParkDetail((s) => s.selectedCarPark)!;
+
+  return (
+    <div className='py-4'>
+      <div className='mb-4 flex items-center gap-1.5'>
+        <Icons.Money className='mt-0.5 text-al-slate-dark' />
+        <div className='text-base font-bold'>요금 정보</div>
+      </div>
+      <div className='flex flex-col gap-2'>
+        {!selectedCarPark.defaultFeeAmount && !selectedCarPark.additionFeeAmount && (
+          <span className='text-sm'>없음</span>
+        )}
+        {selectedCarPark.defaultFeeAmount && (
+          <div className=' flex flex-col gap-2'>
+            <div className='flex items-center text-sm'>
+              <div className=''>기본 요금(시간)</div>
+              <div className='ml-auto flex items-center gap-1.5'>
+                <span>{selectedCarPark.defaultFeeAmount}원</span>
+                <Separator />
+                <span>{selectedCarPark.defaultFeeTime}분</span>
+              </div>
+            </div>
           </div>
         )}
-        <div className='flex items-center py-2 px-5'>
-          <Icons.Location className='text-al-slate' />
-          <div className='ml-1 text-sm'>{selectedCarPark.address_name}</div>
-          <Button
-            className='ml-auto rounded-full'
-            size='xs'
-            variant='subtle'
-            onClick={navToKakaoMap}
-          >
-            지도보기
-          </Button>
-        </div>
-        <div className='flex items-center py-2 px-5'>
-          <Icons.Route className='text-al-slate' />
-          <div className='ml-1 text-sm'>걸어서 업체가는 방법</div>
-          <Button
-            className='ml-auto rounded-full'
-            size='xs'
-            variant='subtle'
-            onClick={navToCompany}
-          >
-            카카오맵
-          </Button>
-        </div>
-      </div>
-
-      <div className='mt-2 bg-white px-5 pt-1 pb-6'>
-        <div className='space:border-t space:border-al-border'>
-          <div className='py-4'>
-            <div className='mb-4 flex items-center gap-1.5'>
-              <Icons.Money className='mt-0.5 text-al-slate-dark' />
-              <div className='text-base font-bold'>요금 정보</div>
-            </div>
-            <div className='flex flex-col gap-2'>
-              {selectedCarPark.defaultFeeAmount && (
-                <div className=' flex flex-col gap-2'>
-                  <div className='flex items-center text-sm'>
-                    <div className=''>기본 요금(시간)</div>
-                    <div className='ml-auto flex items-center gap-1.5'>
-                      <span>{selectedCarPark.defaultFeeAmount}원</span>
-                      <Separator />
-                      <span>{selectedCarPark.defaultFeeTime}분</span>
-                    </div>
-                  </div>
-                </div>
-              )}
-              {selectedCarPark.additionFeeAmount && (
-                <div className=' flex flex-col gap-2'>
-                  <div className='flex items-center text-sm'>
-                    <div className=''>추가 요금(시간)</div>
-                    <div className='ml-auto flex items-center gap-1.5'>
-                      <span>{selectedCarPark.additionFeeAmount}원</span>
-                      <Separator />
-                      <span>{selectedCarPark.additionFeeTime}분</span>
-                    </div>
-                  </div>
-                </div>
-              )}
+        {selectedCarPark.additionFeeAmount && (
+          <div className=' flex flex-col gap-2'>
+            <div className='flex items-center text-sm'>
+              <div className=''>추가 요금(시간)</div>
+              <div className='ml-auto flex items-center gap-1.5'>
+                <span>{selectedCarPark.additionFeeAmount}원</span>
+                <Separator />
+                <span>{selectedCarPark.additionFeeTime}분</span>
+              </div>
             </div>
           </div>
-
-          <TimeInfo />
-
-          <div className='py-4'>
-            <div className='mb-4 flex items-center gap-1.5'>
-              <Icons.File className='mt-0.5 text-al-slate-dark' />
-              <div className='text-base font-bold'>기타 정보</div>
-            </div>
-            <pre className='whitespace-pre-wrap font-sans text-sm leading-[22px]'>
-              {selectedCarPark.otherInfo ?? '없음'}
-            </pre>
-          </div>
-        </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
@@ -193,6 +196,9 @@ function TimeInfo() {
         <div className='text-base font-bold'>운영 정보</div>
       </div>
       <div className='flex flex-col gap-2'>
+        {!weekdayTimeRange && !weekendTimeRange && !holidayTimeRange && (
+          <span className='text-sm'>없음</span>
+        )}
         {weekdayTimeRange && (
           <div className='flex items-center text-sm'>
             <div className=''>평일</div>
@@ -212,6 +218,22 @@ function TimeInfo() {
           </div>
         )}
       </div>
+    </div>
+  );
+}
+
+function OtherInfo() {
+  const selectedCarPark = useCarParkDetail((s) => s.selectedCarPark)!;
+
+  return (
+    <div className='py-4'>
+      <div className='mb-4 flex items-center gap-1.5'>
+        <Icons.File className='mt-0.5 text-al-slate-dark' />
+        <div className='text-base font-bold'>기타 정보</div>
+      </div>
+      <pre className='whitespace-pre-wrap font-sans text-sm leading-[22px]'>
+        {selectedCarPark.otherInfo ?? '없음'}
+      </pre>
     </div>
   );
 }
