87b238f03db1925e404426bdd5af2adb377c0eff
edward <bepyan@naver.com>
2023-04-16 20:49:19 +0900

    ✨ sort public-parking-lot

--- src/services/seoul-parking-lot.ts
+++ src/services/seoul-parking-lot.ts
@@ -6,7 +6,9 @@ import { DATA } from '../assets/seoul-parking-lot.json';
 const db = DATA as unknown as SeoulParkingPlace[];
 
 export const getSeoulPublicParkingLotList = (search: string) => {
-  return db.filter(
-    (parkingLot) => parkingLot.addr.includes(search) || parkingLot.parking_name.includes(search),
-  );
+  return db
+    .filter(
+      (parkingLot) => parkingLot.addr.includes(search) || parkingLot.parking_name.includes(search),
+    )
+    .sort((a, b) => (a.parking_name > b.parking_name ? 1 : -1));
 };
